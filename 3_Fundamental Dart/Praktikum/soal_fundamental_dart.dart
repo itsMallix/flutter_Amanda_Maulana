6e3c2c2a1e1291921b874cae40246d199f4d8c50
itsMallix <amandamaulana00@gmail.com>
2023-08-18 11:10:41 +0700

    add file soal materi dart

---
+++ 3_Fundamental Dart/Praktikum/soal_fundamental_dart.dart
@@ -0,0 +1,20 @@
+// 1.  Implementasikan rumus lingkaran pada program dart
+// Soal nomor 1
+void main() {
+  int jari = 12;
+  double phi = 3.14;
+
+  double luas_lingkaran = phi * jari * jari;
+
+  print("Luas lingkaran adalah $luas_lingkaran");
+
+/* 2. Buatlah 3 Buah variabel yang berisi String, lalu 
+sambungkan seluruh string tersebut dan tampilkan pada layar */
+
+  String kata1 = "Mohon ajarin";
+  String kata2 = "Dong puh";
+  String kata3 = "Sepuhh";
+
+  String kata_gabungan = "$kata1 $kata2 $kata3";
+  print(kata_gabungan);
+}
