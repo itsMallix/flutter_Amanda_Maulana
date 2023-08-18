8f55d36c7c305a4dad29407db755c15eb46f6266
itsMallix <amandamaulana00@gmail.com>
2023-08-18 10:08:23 +0700

    add file dart

---
+++ 3_Fundamental Dart/Praktikum/fundamental_dart.dart
@@ -0,0 +1,19 @@
+// Fungsi main
+void main() {
+  // Ini komentar
+  print("Hello World");
+
+  // ini variabel
+  var usia = 19;
+  print(usia);
+
+  //ini tipedata
+  String nama = "maulana";
+  double tinggi = 173.3;
+  int umur = 20;
+  bool single = false;
+
+  // Operator
+  print(umur > 14);
+  print(umur == 20 || umur >= 20);
+}
---
+++ 3_Fundamental Dart/Praktikum/konstanta_dart.dart
@@ -0,0 +1,11 @@
+void main() {
+  //ini konstanta
+  final usia = 99;
+  print(usia);
+
+  print(1 + 1);
+  print(1 - 2);
+  print(1 * 3);
+  print(1 / 4);
+  print(1 % 5);
+}
