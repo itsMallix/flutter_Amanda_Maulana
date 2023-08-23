0043b1ba9034ca26b4bf77bfd943b478c8696631
itsMallix <amandamaulana00@gmail.com>
2023-08-23 11:04:42 +0700

    refactor soal

--- 4_Branching - Looping - Function/Praktikum/soal_eksplorasi.dart
+++ 4_Branching - Looping - Function/Praktikum/soal_eksplorasi.dart
@@ -1,21 +1,30 @@
-bool bilagnanPrima(int n) {
-  if (n <= 1) {
-    return false;
+import 'dart:io';
+
+// NO 1
+cekBilanganPrima() {
+  stdout.write("Masukkan Bilangan Yang Ingin Di Cek : ");
+  var input = stdin.readLineSync();
+  int bilangan = int.parse(input!);
+
+  if (bilangan <= 1) {
+    stdout.write("$bilangan bukan bilangan prima");
   }
-  if (n <= 3) {
-    return true;
+  if (bilangan == 2) {
+    stdout.write("$bilangan adalah bilangan prima");
   }
-  if (n % 2 == 0 || n % 3 == 0) {
-    return false;
-  }
-  int i = 5;
-  while (i * i <= n) {
-    if (n % i == 0 || n % (i + 2) == 0) {
-      return false;
+  if (bilangan > 1) {
+    for (int i = 2; i < bilangan;) {
+      if (bilangan % i == 0) {
+        stdout.write("$bilangan bukan bilangan prima");
+        break;
+      } else {
+        stdout.write("$bilangan adalah bilangan prima");
+        break;
+      }
     }
-    i += 6;
   }
-  return true;
 }
 
-void main() {}
+void main() {
+  cekBilanganPrima();
+}
