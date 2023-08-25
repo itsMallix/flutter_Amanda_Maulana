f631f845a66cb213e94b35acb139f7e86df541e2
itsMallix <amandamaulana00@gmail.com>
2023-08-25 08:55:27 +0700

    docs challange dart

--- 4_Branching - Looping - Function/Praktikum/challange.dart
+++ 4_Branching - Looping - Function/Praktikum/challange.dart
@@ -1,4 +1,7 @@
 //  membuat fungtion untuk menghitung jumlah barang yang sudah kita beli dari sebuah toko
+import 'dart:html';
+import 'dart:io';
+
 var buku = 10000;
 var pensil = 5000;
 var tas = 100000;
@@ -19,4 +22,12 @@ void main() {
   print(totalBarang(buku, pensil, tas));
   print(diskon(buku, pensil, tas));
   print(tagihan(buku, pensil, tas));
+
+  bool cond = true;
+  do {
+    stdout.write("coba do while");
+  } while (cond != false);
+
+  var currentDate = DateTime.now();
+  print(currentDate.weekday);
 }
