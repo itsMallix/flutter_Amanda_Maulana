36ce2caf2c80bc44b064808f725cbbaf055b8479
itsMallix <amandamaulana00@gmail.com>
2023-08-27 22:39:02 +0700

    docs soal prioritas 2

--- 5_Advance Function - Async - Await - Collection/Praktikum/soal prioritas_2.dart
+++ 5_Advance Function - Async - Await - Collection/Praktikum/soal prioritas_2.dart
@@ -2,12 +2,15 @@
 
 soalPrioritas2_no1() {
   List<List<String>> listData = [
+    //formatting list
     ["Hero", "Benedetta"],
     ["Role", "Assasin/Fighter"],
     ["MMR", "2358"]
   ];
 
-  Map listDataMap = {for (var i in listData) i[0]: i[1]};
+  Map listDataMap = {
+    for (var i in listData) i[0]: i[1]
+  }; // lakukan perulangan dan definisikan elemen mana yang akan jadi key
   print(listDataMap);
   print("=" * 25);
 }
@@ -16,13 +19,14 @@ soalPrioritas2_no2() {
   List<int> sampleInput = [1, 2, 3, 5, 5, 7];
 
   int sum = 0;
-  sampleInput.forEach((value) => sum += value);
+  sampleInput.forEach((value) => sum += value); //cari total nilai dari list
 
-  double avg = sum / sampleInput.length;
-  int result = avg.ceil();
+  double avg = sum / sampleInput.length; // cari rata-rata
+  int result = avg.ceil(); // pembulatan ke atas
 
   print("Rata-rata = $avg");
   print("Hasil = $result");
+  print("=" * 25);
 }
 
 void main() {
