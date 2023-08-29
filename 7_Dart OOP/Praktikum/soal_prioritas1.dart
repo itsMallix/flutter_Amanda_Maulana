f01e483bebece4ec2998fd6d925f5c020fb67944
itsMallix <amandamaulana00@gmail.com>
2023-08-29 10:07:59 +0700

    docs soal prioritas 1

---
+++ 7_Dart OOP/Praktikum/soal_prioritas1.dart
@@ -0,0 +1,38 @@
+class Hewan {
+  int _berat = 0;
+  int get berat => _berat;
+  set berat(int value) => _berat = value;
+
+  Hewan(this._berat);
+}
+
+class Mobil {
+  int kapasitas = 10;
+  List<Hewan> muatan = [];
+
+  void tambahanMuatan(List<Hewan> hewan) {
+    for (var hewan in hewan) {
+      if (hewan.berat <= kapasitas) {
+        muatan.add(hewan);
+      }
+    }
+  }
+
+  void totalMuatan() {
+    print(
+      "Kapasitas maximum: $kapasitas\nTerisi : ${muatan.length}",
+    );
+  }
+}
+
+void main() {
+  var ayam = Hewan(1);
+  var sapi = Hewan(3);
+  var kambing = Hewan(2);
+  var dino = Hewan(20);
+  var megalodon = Hewan(100);
+
+  var mobil = Mobil();
+  mobil.tambahanMuatan([ayam, sapi, kambing, dino, megalodon]);
+  mobil.totalMuatan();
+}
