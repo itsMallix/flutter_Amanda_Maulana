37cee63d1db3a7611396ffb3f282b85f56e848fc
itsMallix <amandamaulana00@gmail.com>
2023-08-30 09:19:26 +0700

    docs soal prioritas 2

--- 7_Dart OOP/Praktikum/soal_prioritas2.dart
+++ 7_Dart OOP/Praktikum/soal_prioritas2.dart
@@ -27,15 +27,37 @@ class Calculator {
 }
 
 class Student {
-  String name = "";
-  String kelas = "";
-  List<Course> course = [];
+  String name;
+  String kelas;
+  List<Course> courses = [];
+
+  Student({required this.name, required this.kelas});
+
   void tampilkanCourse() {
-    print(course);
+    print("Courses milik $name kelas $kelas:");
+    for (var i = 0; i < courses.length; i++) {
+      var course = courses[i];
+      print("${i + 1}. ${course.judul} - ${course.deskripsi}");
+    }
+  }
+
+  void tambahCourse(Course newCourse) {
+    courses.add(newCourse);
+    // print("Course ${newCourse.judul} berhasil ditambahkan");
+  }
+
+  void hapusCourse(Course delCourse) {
+    courses.remove(delCourse);
+    print("Course ${delCourse.judul} berhasil dihapus");
   }
 }
 
-class Course {}
+class Course {
+  String judul;
+  String deskripsi;
+
+  Course({required this.judul, required this.deskripsi});
+}
 
 void main() {
   int angka1 = 4;
@@ -46,7 +68,18 @@ void main() {
   calculator.perkalian();
   calculator.pembagian();
 
-  var student1 = Student();
-  student1.name = "amanda";
-  student1.kelas = "Kelas C";
+  Student student1 = new Student(name: "amanda", kelas: "Flutter-C");
+  student1.tampilkanCourse();
+
+  Course course1 = new Course(judul: "Flutter", deskripsi: "Belajar Flutter");
+  Course course2 = new Course(judul: "UI/UX", deskripsi: "Belajar Ui/Ux");
+  Course course3 = new Course(judul: "Golang", deskripsi: "Belajar golang");
+
+  student1.tambahCourse(course1);
+  student1.tambahCourse(course2);
+  student1.tambahCourse(course3);
+
+  student1.tampilkanCourse();
+  student1.hapusCourse(course2);
+  student1.tampilkanCourse();
 }
