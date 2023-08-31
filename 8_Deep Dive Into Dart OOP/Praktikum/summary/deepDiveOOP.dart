09a4349d3e458b9bb40cf7e5d710f3c38b00b972
itsMallix <amandamaulana00@gmail.com>
2023-08-31 23:01:54 +0700

    summary

---
+++ 8_Deep Dive Into Dart OOP/Praktikum/summary/deepDiveOOP.dart
@@ -0,0 +1,16 @@
+// constructor
+class Hewan {
+  int? mata;
+  int? kaki;
+
+  Hewan() {
+    mata = 0;
+    kaki = 0;
+  }
+}
+
+void main() {
+  var hewan = Hewan();
+  print(hewan.mata);
+  print(hewan.kaki);
+}
